cd6cd49ce5aa6bb059419604ac40fe890abf87fa
RomanSinAlexandra <romio04pro@gmail.com>
2026-04-08 00:43:10 +0200

    laba2

---
+++ src/components/molecules/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header style={{ padding: "16px", background: "#4f8cff", color: "white" }}>
+      <h1>Мій застосунок</h1>
+    </header>
+  );
+};
+
+export default Header;
